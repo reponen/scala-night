7df28efcc75fb9996f53dd3bd4cda87e3d5d47ce
Sampo Reponen <samporeponen@gmail.com>
2013-02-18 17:11:45 +0200

    added couple of basic list methods

--- src/main/scala/fi/kilosoft/scalanight/Lists.scala
+++ src/main/scala/fi/kilosoft/scalanight/Lists.scala
@@ -3,6 +3,11 @@ package fi.kilosoft.scalanight
 import common._
 
 object Lists {
+  def head(inLst: List[Int]): Int = inLst.head
+
+  def tail(inLst: List[Int]): Int = ???
+  
+  // :: creates a new list
   def odd(inLst: List[Int]): List[Int] = {
     if (inLst == Nil) Nil
     else if (inLst.head % 2 == 1) inLst.head :: odd(inLst.tail)
--- src/test/scala/fi/kilosoft/scalanight/ListsSuite.scala
+++ src/test/scala/fi/kilosoft/scalanight/ListsSuite.scala
@@ -7,11 +7,21 @@ import org.scalatest.junit.JUnitRunner
 @RunWith(classOf[JUnitRunner])
 class ListSuite extends FunSuite {
  
+  test("head() returns the first element of a list") {
+    val list = List(1,2,3,4,5)
+    assert(Lists.head(list) === 1)
+  }
+  
+  test("tail() returns the first element of a list") {
+    val list = List(1,2,3,4,5)
+    assert(Lists.tail(list) === 5)
+  }
+
   test("odd() returns only odd numbers") {
     val lst=List(1,7,2,8,5,6,3,9,14,12,4,10)
     assert(Lists.odd(lst) === List(1,7,5,3,9))
   }
- 
+
   test("even () returns only even numbers") {
     val lst=List(1,7,2,8,5,6,3,9,14,12,4,10)
     assert(Lists.even(lst) === List(2,8,6,14,12,4,10))
